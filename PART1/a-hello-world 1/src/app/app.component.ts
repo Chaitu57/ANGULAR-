6791e60d88d5fc57705ecf2b53f4639052bd82c8
Chaitu57 <102200591+Chaitu57@users.noreply.github.com>
2022-06-03 06:28:43 -0700

    Add files via upload

---
+++ PART1/a-hello-world 1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'chaitanya raykar';
+   display(){
+      return "we work for mindgate solution pvt ltd";
+   }
+}
---
+++ PART1/d-interpolation-angular 1/src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer:boolean=true;
+  serverCreationStatus: string="no server was created";
+  serverName:string="";
+
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer=false;   
+    }, 5000);
+   }
+
+onCreateServer(){
+  this.serverCreationStatus="server was created!!";
+}
+ 
+// onUpdateServerName(event:Event){
+
+//   this.serverName=(<HTMLInputElement> event.target).value;
+//   }
+  ngOnInit(): void {
+  }
+
+}
